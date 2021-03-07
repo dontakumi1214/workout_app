3bb6a8d083b5097272ab7b25990f585b95abbd4c
dontakumi1214 <condotakumi@gmail.com>
2021-03-07 03:55:15 +0000

    3/7 commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_023057) do
+ActiveRecord::Schema.define(version: 2021_03_06_083329) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
